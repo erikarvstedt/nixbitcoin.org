93a2944a371db2a5485bca45608ae0407ad8e813
nixbitcoin <nixbitcoin@i2pmail.org>
2021-03-25 13:11:54 +0000

    add node config

---
+++ nix-bitcoin-release.nix
@@ -0,0 +1,4 @@
+{
+  url = "https://github.com/fort-nix/nix-bitcoin/releases/download/v0.0.41/nix-bitcoin-0.0.41.tar.gz";
+  sha256 = "5809244c1b57ace63886b0a1e1ab4f5b20f97f9063d7abbd306827ea103b8bce";
+}
