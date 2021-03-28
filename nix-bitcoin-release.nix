2efaf4c05d0222af9c9dfd86a668f836a561beef
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-03-28 21:30:19 +0200

    [TMP] use nix-bitcoin PR #348 as release version
    https://github.com/fort-nix/nix-bitcoin/pull/348

--- nix-bitcoin-release.nix
+++ nix-bitcoin-release.nix
@@ -1,4 +1,4 @@
-{
-  url = "https://github.com/fort-nix/nix-bitcoin/releases/download/v0.0.41/nix-bitcoin-0.0.41.tar.gz";
-  sha256 = "5809244c1b57ace63886b0a1e1ab4f5b20f97f9063d7abbd306827ea103b8bce";
+builtins.fetchTarball {
+  url = "https://github.com/erikarvstedt/nix-bitcoin/archive/run-tests-vm.tar.gz";
+  sha256 = "08471sqznjinjl9wdgm68kna47a89b06zca6yl83jm9fbp1ddr9j";
 }
