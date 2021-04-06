63669b32beb079ecec620c07b90d53bc64f7015e
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-04-06 22:06:50 +0200

    use nix-bitcoin 0.0.42

--- nix-bitcoin-release.nix
+++ nix-bitcoin-release.nix
@@ -1,4 +1,4 @@
-builtins.fetchTarball {
-  url = "https://github.com/erikarvstedt/nix-bitcoin/archive/run-tests-vm.tar.gz";
-  sha256 = "08471sqznjinjl9wdgm68kna47a89b06zca6yl83jm9fbp1ddr9j";
+{
+  url = "https://github.com/fort-nix/nix-bitcoin/releases/download/v0.0.42/nix-bitcoin-0.0.42.tar.gz";
+  sha256 = "08i207nijk1b2fvd027hwmfb57667aqpc5apxmvcq3i6zq8mn0vz";
 }
